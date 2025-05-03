90e9d99c55213656e7a179ae415ed6ef70732728
devinschumacher <devin@serp.co>
2025-05-03 09:37:36 -0700

    customize brand

--- app/app.vue
+++ app/app.vue
@@ -20,8 +20,8 @@ const appear = ref(false)
 const appeared = ref(false)
 
 useSeoMeta({
-  ogImage: 'https://assets.hub.nuxt.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL2xhbmRpbmctdGVtcGxhdGUubnV4dC5kZXYiLCJpYXQiOjE3Mzk0NjMzNzV9.ja2nUDVOoIFvyaMmg9Jn51uNMoYYt4WA1KWUQBWwUPo.jpg?theme=light',
-  twitterImage: 'https://assets.hub.nuxt.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL2xhbmRpbmctdGVtcGxhdGUubnV4dC5kZXYiLCJpYXQiOjE3Mzk0NjMzNzV9.ja2nUDVOoIFvyaMmg9Jn51uNMoYYt4WA1KWUQBWwUPo.jpg?theme=light',
+  ogImage: 'https://images.serp.co/SERP.png',
+  twitterImage: 'https://images.serp.co/SERP.png',
   twitterCard: 'summary_large_image'
 })
 
