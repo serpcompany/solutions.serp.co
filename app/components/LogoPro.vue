46d11f433751ee167f3721331278593b4a78cc61
devinschumacher <devin@serp.co>
2025-05-03 15:39:01 -0700

    Update the color mode switching of the SVG.

--- app/components/LogoPro.vue
+++ app/components/LogoPro.vue
@@ -1,9 +1,11 @@
 <template>
-  <img
-    :src="`/logos/serp-wordmark-${colorMode.value === 'dark' ? 'light' : 'dark'}.svg`"
-    alt="SERP"
-    class="h-[30px] object-contain"
-  />
+  <ClientOnly>
+    <img
+      :src="`/logos/serp-wordmark-${colorMode.value === 'dark' ? 'light' : 'dark'}.svg`"
+      alt="SERP"
+      class="h-[30px] object-contain"
+    />
+  </ClientOnly>
 </template>
 
 <script setup>
